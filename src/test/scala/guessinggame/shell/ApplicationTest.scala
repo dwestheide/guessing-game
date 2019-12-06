bd11ea3967058886edf9a71d0e3e2eb6aa66d362
Daniel Westheide <dwestheide@googlemail.com>
2019-12-06 11:51:21 +0100

    Add assertion to ApplicationTest

--- src/test/scala/guessinggame/shell/ApplicationTest.scala
+++ src/test/scala/guessinggame/shell/ApplicationTest.scala
@@ -1,15 +1,24 @@
 package guessinggame.shell
 
 import minitest.SimpleTestSuite
+import scala.io.Source
 
 object ApplicationTest extends SimpleTestSuite {
   test("Running the application") {
-    val stream = new CapturingPrintStream
-    val messagePrinter = new MessagePrinter(stream.out)
+    val stream                   = new CapturingPrintStream
+    val messagePrinter           = new MessagePrinter(stream.out)
     val inputReader: InputReader = _ => "42"
-    val randomGenerator = () => 42
-    val application = new Application(messagePrinter, inputReader, randomGenerator)
+    val randomGenerator          = () => 42
+    val application =
+      new Application(messagePrinter, inputReader, randomGenerator)
+
     application.run()
-    // println(stream.capturedOutput)
+
+    val expectedOutput = Source
+      .fromURL(getClass().getResource("/expected-output.txt"))
+      .getLines()
+      .mkString("\n")
+
+    assertEquals(stream.capturedOutput, expectedOutput)
   }
 }
